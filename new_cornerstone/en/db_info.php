1f626fd2f28e0fa91f34862036f94d6603616213
sm1215 <macuser@Sam.local>
2014-01-27 11:03:56 -0500

    db info
    updated test db info

--- new_cornerstone/en/db_info.php
+++ new_cornerstone/en/db_info.php
@@ -7,9 +7,15 @@ $db = "trekcornerstone";
 */
 //New Database info
 
-$hname =  "trekcsdb.db.8473798.hostedresource.com";
-$uname = "trekcsdb";
-$pass = "Print@101";
-$db = "trekcsdb";
+$hname =  "192.186.252.201";
+$uname = "psadmin";
+$pass = "ps123";
+$db = "pswork";
+
+/* Test user credentials 
+*	Username: test1
+*	Password: 123
+*/
+
 
 ?>
